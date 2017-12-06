cf0380234549e7caf16222dbaabee3ff12eb208b
dkoukoul <dimitris@commonslab.gr>
2017-12-07 00:06:13 +0200

    Created CustomViewPager to disable swipe between tabs. Created different fragment classes for each tab. Added DatePicker to work only on "From-To" tab.

--- app/src/main/java/gr/commonslab/plirosousosta/EntitledActivity.java
+++ app/src/main/java/gr/commonslab/plirosousosta/EntitledActivity.java
@@ -1,40 +1,32 @@
 package gr.commonslab.plirosousosta;
 
+import android.app.Activity;
 import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.view.ViewPager;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewDebug;
 import android.view.ViewGroup;
-
 import android.widget.Button;
 import android.widget.DatePicker;
-import android.widget.ImageButton;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
 import java.util.Calendar;
-import java.util.List;
 import java.util.Locale;
-import java.util.Vector;
 
 
 public class EntitledActivity extends AppCompatActivity {
@@ -52,7 +44,7 @@ public class EntitledActivity extends AppCompatActivity {
     /**
      * The {@link ViewPager} that will host the section contents.
      */
-    private ViewPager mViewPager;
+    private CustomViewPager mViewPager;
     private String currentMonth;
     private String currentYear;
     private TabLayout tabLayout;
@@ -60,6 +52,7 @@ public class EntitledActivity extends AppCompatActivity {
     private static Calendar ToDate;
     public static boolean bToDate = false;
     private static Button button_info;
+    private static View currentView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -73,7 +66,8 @@ public class EntitledActivity extends AppCompatActivity {
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container_entitled);
+        mViewPager = (CustomViewPager) findViewById(R.id.container_entitled);
+        mViewPager.setPagingEnabled(false);
         mViewPager.setAdapter(mSectionsPagerAdapter);
         tabLayout = (TabLayout) findViewById(R.id.tabs_entitled);
         tabLayout.setupWithViewPager(mViewPager);
@@ -81,15 +75,6 @@ public class EntitledActivity extends AppCompatActivity {
         FromDate = Calendar.getInstance();
         ToDate = Calendar.getInstance();
     }
-
-    /*
-    public class MonthFragment extends Fragment {
-        @Override
-        public View onCreateView(){
-
-        }
-    }
-    /**/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -114,35 +99,42 @@ public class EntitledActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
+     * A placeholder fragment containing Month view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class MonthFragment extends Fragment {
         /**
          * The fragment argument representing the section number for this
          * fragment.
          */
         private static final String ARG_SECTION_NUMBER = "section_number";
 
-        public PlaceholderFragment() {
-
+        public MonthFragment(int sectionNumber) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            this.setArguments(args);
         }
 
         /**
          * Returns a new instance of this fragment for the given section
          * number.
          */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
+        public static MonthFragment newInstance(int sectionNumber) {
+            MonthFragment fragment = new MonthFragment(sectionNumber);
+            /**
             Bundle args = new Bundle();
             args.putInt(ARG_SECTION_NUMBER, sectionNumber);
             fragment.setArguments(args);
+             /**/
             return fragment;
+
         }
 
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_month_entitled, container, false);
+            setMonthValues(rootView);
+            /**
             switch (getArguments().getInt(ARG_SECTION_NUMBER)) {
                 case 0:
                 case 1://MONTH
@@ -154,20 +146,173 @@ public class EntitledActivity extends AppCompatActivity {
                     setAnnualValues(rootView);
                     break;
                 case 3://FROM-TO
-                    showDatePicker(this.getActivity());
                     rootView = inflater.inflate(R.layout.fragment_fromto_entitled, container, false);
-                    setFromToValues(rootView, FromDate, ToDate);
+                    currentView = rootView;
+                    showDatePicker(this.getActivity());
                     break;
                 case 4://ALL
                     rootView = inflater.inflate(R.layout.fragment_all_entitled, container, false);
                     setAllValues(rootView);
                     break;
+                default:
+                    rootView = inflater.inflate(R.layout.fragment_month_entitled, container, false);
+                    setMonthValues(rootView);
+                    break;
             }
+             /**/
             button_info = (Button) rootView.findViewById(R.id.button_whatcanido);
             button_info.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    //Go to entitled activity
+                    //Go to INFO activity
+                    Intent intent_info = new Intent(v.getContext(), InfoActivity.class);
+                    v.getContext().startActivity(intent_info);
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    public static class YearFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public YearFragment(int sectionNumber) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            this.setArguments(args);
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static YearFragment newInstance(int sectionNumber) {
+            YearFragment fragment = new YearFragment(sectionNumber);
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_year_entitled, container, false);
+            setAnnualValues(rootView);
+            button_info = (Button) rootView.findViewById(R.id.button_whatcanido);
+            button_info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Go to INFO activity
+                    Intent intent_info = new Intent(v.getContext(), InfoActivity.class);
+                    v.getContext().startActivity(intent_info);
+                }
+            });
+
+            return rootView;
+        }
+    }
+
+    /**
+     * A placeholder fragment containing Month view.
+     */
+    public static class FromToFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public FromToFragment(int sectionNumber) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            this.setArguments(args);
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static FromToFragment newInstance(int sectionNumber) {
+            FromToFragment fragment = new FromToFragment(sectionNumber);
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_fromto_entitled, container, false);
+            //setMonthValues(rootView);
+            currentView = rootView;
+            setUserVisibleHint(false);
+            button_info = (Button) rootView.findViewById(R.id.button_whatcanido);
+            button_info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Go to INFO activity
+                    Intent intent_info = new Intent(v.getContext(), InfoActivity.class);
+                    v.getContext().startActivity(intent_info);
+                }
+            });
+
+            return rootView;
+        }
+
+        @Override
+        public void setMenuVisibility(final boolean visible) {
+            super.setMenuVisibility(visible);
+            if(visible) {
+                showDatePicker(getActivity());
+            }
+        }
+    }
+
+    /**
+     * A placeholder fragment containing Month view.
+     */
+    public static class AllFragment extends Fragment {
+        /**
+         * The fragment argument representing the section number for this
+         * fragment.
+         */
+        private static final String ARG_SECTION_NUMBER = "section_number";
+
+        public AllFragment(int sectionNumber) {
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            this.setArguments(args);
+        }
+
+        /**
+         * Returns a new instance of this fragment for the given section
+         * number.
+         */
+        public static AllFragment newInstance(int sectionNumber) {
+            AllFragment fragment = new AllFragment(sectionNumber);
+            Bundle args = new Bundle();
+            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_all_entitled, container, false);
+
+            button_info = (Button) rootView.findViewById(R.id.button_whatcanido);
+            button_info.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //Go to INFO activity
                     Intent intent_info = new Intent(v.getContext(), InfoActivity.class);
                     v.getContext().startActivity(intent_info);
                 }
@@ -191,8 +336,23 @@ public class EntitledActivity extends AppCompatActivity {
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
             // Return a PlaceholderFragment (defined as a static inner class below).
-
-            return PlaceholderFragment.newInstance(position);
+            switch (position) {
+                case 0:
+                    MonthFragment tab1 = new MonthFragment(0);
+                    return tab1;
+                case 1:
+                    YearFragment tab2 = new YearFragment(1);
+                    return tab2;
+                case 2:
+                    FromToFragment tab3 = new FromToFragment(2);
+                    return tab3;
+                case 3:
+                    AllFragment tab4 = new AllFragment(3);
+                    return tab4;
+                default:
+                    return null;
+            }
+            //return PlaceholderFragment.newInstance(position);
         }
 
         @Override
@@ -219,7 +379,7 @@ public class EntitledActivity extends AppCompatActivity {
         }
     }
 
-    public static void showDatePicker(FragmentActivity activity){
+    public static void showDatePicker(Activity activity){
         DialogFragment dateFragment;
         dateFragment = new DatePickerFragment();
         dateFragment.show(activity.getFragmentManager(), "datePicker");
@@ -261,7 +421,9 @@ public class EntitledActivity extends AppCompatActivity {
                 ToDate.set(Calendar.DATE, day);
                 bToDate = false;
                 //TODO: set values
-                setFromToValues(this.getView(),FromDate, ToDate);
+                if(currentView != null) {
+                    setFromToValues(currentView,FromDate, ToDate);
+                }
             } else {
                 FromDate.set(Calendar.HOUR, 0);
                 FromDate.set(Calendar.MINUTE, 0);
@@ -283,11 +445,32 @@ public class EntitledActivity extends AppCompatActivity {
     }
 
     public static void setFromToValues(View rootView, Calendar From, Calendar to) {
+        DBHelper dbHelper = new DBHelper(rootView.getContext());
+        SQLiteDatabase sqldb = dbHelper.getReadableDatabase();
+        String s = "";
         //TODO: implement setFromToValues
+        //TOP
+        TextView text_total_amount = rootView.findViewById(R.id.text_total_fromto_amount);
+
+        s = "€23.45";
+        text_total_amount.setText(s);
     }
 
     public static void setAnnualValues(View rootView) {
         //TODO: implement setAnnualValues
+        int month = Calendar.getInstance().get(Calendar.MONTH);
+        float hours = 0f;
+        int minutes = 0;
+        float amount = 0f;
+        String s = "";
+        DBHelper dbHelper = new DBHelper(rootView.getContext());
+        SQLiteDatabase sqldb = dbHelper.getReadableDatabase();
+
+        //TOP
+        TextView text_total_amount = rootView.findViewById(R.id.text_total_amount);
+        //amount = dbHelper.getEntitledPaymentinMonth(month);
+        s = "TESTING YEAR";
+        text_total_amount.setText(s);
     }
 
     public static void setMonthValues(View rootView) {
