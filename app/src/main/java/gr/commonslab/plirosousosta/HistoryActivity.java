76f683adeb1b3ee4562525adc2f92fa27607a502
dkoukoul <dimitris@commonslab.gr>
2018-02-28 16:09:48 +0200

    Enabled MAX date to date picker.

--- app/src/main/java/gr/commonslab/plirosousosta/HistoryActivity.java
+++ app/src/main/java/gr/commonslab/plirosousosta/HistoryActivity.java
@@ -349,8 +349,7 @@ public class HistoryActivity extends AppCompatActivity {
 
             // Create a new instance of DatePickerDialog and return it
             DatePickerDialog dp = new DatePickerDialog(getActivity(), R.style.MySpinnerPickerStyle , this, year, month, day);
-            //TODO: REMOVE SET MAX
-            // dp.getDatePicker().setMaxDate(System.currentTimeMillis());
+            dp.getDatePicker().setMaxDate(System.currentTimeMillis());
             if (bToDate) {
                 dp.setTitle(getString(R.string.picker_title_to));
             } else {
